6ca220223d20ff8dcf97c35331a3e1bb28ae1e97
Willian Haas <willian@domain7.com>
2018-08-29 22:28:24 -0700

    Finished lecture 95

--- auth-graphql-starter-master/client/index.js
+++ auth-graphql-starter-master/client/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
+import LoginForm from './components/LoginForm';
+
+const networkInterface = createNetworkInterface({
+  // whenever a custom network interface is created, is it necessary to specify the uri
+  uri: '/graphql',
+  opts: {
+    // send along cookies whenever a request is made to the backend server
+    credentials: 'same-origin'
+  }
+});
 
 const client = new ApolloClient({
+  networkInterface,
   // uniquely identifies every fetched record
   dataIdFromObject: o => o.id
 });
@@ -15,7 +26,9 @@ const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path="/" component={App}></Route>
+        <Route path="/" component={App}>
+          <Route path="login" component={LoginForm} />
+        </Route>
       </Router>
     </ApolloProvider>
   );
